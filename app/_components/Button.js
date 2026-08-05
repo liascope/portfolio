2a4e619f198df95e5315be9573eee7868af06778
liaakin <lia.07@live.de>
2026-08-06 01:06:21 +0200

    style:fix Button size on small screen

--- app/_components/Button.js
+++ app/_components/Button.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
-export default function Button({ link, bright = false, children, target = '_self', textSize = 'sm:my-6 sm:px-6 py-2 my-3 px-3' }) {
+export default function Button({ link, bright = false, children, target = '_self', textSize = 'md:my-6 md:px-6 py-2 my-3 px-3' }) {
   if (bright)
     return (
       <Link
         href={link}
         target={target}
-        className={`bg-white/80 text-teal-800/80 text-sm md:text-base uppercase font-bold shadow-md sm:tracking-wide hover:text-teal-600/80 rounded-lg hover:rounded-sm hover:bg-white transition ${textSize}`}
+        className={`bg-white/80 text-teal-800/80 text-sm md:text-base uppercase font-bold shadow-md md:tracking-wide hover:text-teal-600/80 rounded-lg whitespace-nowrap hover:rounded-sm hover:bg-white transition ${textSize}`}
       >
         {children}
       </Link>
@@ -15,7 +15,7 @@ export default function Button({ link, bright = false, children, target = '_self
     <Link
       href={link}
       target={target}
-      className={`bg-teal-700/80 text-white text-sm md:text-base uppercase font-bold shadow-md sm:tracking-wide rounded-lg hover:bg-teal-600/80 transition hover:rounded-sm ${textSize}`}
+      className={`bg-teal-700/80 text-white text-sm md:text-base uppercase font-bold shadow-md md:tracking-wide rounded-lg hover:bg-teal-600/80 transition hover:rounded-sm whitespace-nowrap ${textSize}`}
     >
       {children}
     </Link>
--- app/_components/Pages/Contact.js
+++ app/_components/Pages/Contact.js
@@ -10,7 +10,7 @@ export default function Contact() {
       <div className="backdrop-blur-lg bg-white/30 rounded-2xl pt-10 px-8 shadow-xl sm:text-lg text-base text-center">
         <p>Interested in working together, have questions, or want to share feedback?</p>
         <p>Feel free to send me an email or connect with me on GitHub.</p>
-        <div className="flex justify-center my-6 space-x-6">
+        <div className="flex justify-center my-6 flex-row gap-2 md:gap-6">
           <Button link={email}>
             <span className="flex flex-row items-center gap-2">
               <Mail />
